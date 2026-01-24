478b64fd46cf266812a53393306dbc5a6621b52b
surafel <surafelmengist2015@gmail.com>
2026-01-24 20:07:56 +0300

    fix the errors about related with deploye

--- services/weatherService.ts
+++ services/weatherService.ts
@@ -1,17 +1,15 @@
 import { api } from "@/lib/axios";
 import { WeatherResponse } from "@/types/weather";
 
-//  API key declared in service layer
-const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-
-//  Safety check (recommended)
-if (!API_KEY) {
-  throw new Error("Missing NEXT_PUBLIC_WEATHER_API_KEY");
-}
-
 export const getWeatherByCity = async (
   city: string
 ): Promise<WeatherResponse> => {
+  const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+
+  if (!API_KEY) {
+    throw new Error("Weather API key is missing");
+  }
+
   const res = await api.get<WeatherResponse>(
     `/weather?q=${city}&units=metric&appid=${API_KEY}`
   );
